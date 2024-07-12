ca6a390610bf429dd13b13509b9076a2e28e1141
ADISRI <design.adisri@gmail.com>
2024-07-12 11:26:55 +0530

    feature

--- src/Components/Cartsection.jsx
+++ src/Components/Cartsection.jsx
@@ -81,7 +81,7 @@ function Cartsection() {
       let price=0
         let books = arraydata.filter((book)=>{
             if (book.id!=id) {
-                price+=book.book_price*book.quantity
+                price+=Math.floor(book.book_price*book.quantity)
                 return book
             }
             
@@ -173,7 +173,7 @@ function Cartsection() {
            <h1 className="text-xl grid grid-cols-2  justify-center m-auto"> <p className=""> Grand Total &nbsp;&nbsp; </p><p>:&#36;{Price} </p> </h1>
         
            
-        <button className="w-1/2 m-auto  overflow-hidden space-x-2 rounded-full text-center justify-center cursor-pointer p-4 bg-purple-600 hover:bg-purple-400 transition-all duration-200 flex">
+        <button className="w-1/2 m-auto  overflow-hidden space-x-2 rounded-full text-center justify-center cursor-pointer p-4 hover:bg-purple-600 bg-purple-400 transition-all duration-200 flex">
         <svg className="" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm0-80h480v-400H240v400Zm240-120q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM360-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80ZM240-160v-400 400Z"/></svg>
         <span className="text-white">Checkout</span></button>
         <br />
